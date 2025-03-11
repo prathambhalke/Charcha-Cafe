e422bf96c77d9b8cfa2ca4a6f9ce353465d00345
Prathamesh Bhalke <98319826+prathambhalke@users.noreply.github.com>
2025-03-11 14:16:12 +0000

    installed the cors npm package in server repo

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 let express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 let authRoutes = require("./routes/auth.route.js"); // Using require here
 let MessageRoutes = require("./routes/message.route.js"); // Using require here
 const { connectDB } = require("./lib/db.js");
@@ -13,6 +14,10 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", MessageRoutes);
