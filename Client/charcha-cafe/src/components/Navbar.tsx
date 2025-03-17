03d7d56eebc69a6f43d7f8dc54edeb101a228f04
Prathamesh Bhalke <98319826+prathambhalke@users.noreply.github.com>
2025-03-17 15:44:09 +0000

    worked on Navbar component

--- Client/charcha-cafe/src/components/Navbar.tsx
+++ Client/charcha-cafe/src/components/Navbar.tsx
@@ -1,7 +1,26 @@
+import { LogOut, Menu, MessageSquare, Settings, User } from "lucide-react"
+
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className="w-full flex items-center justify-between px-12 py-6 btn btn-primary fixed">
+            <div className="flex">
+                <span className="font-mono font-extrabold text-lg mr-3 justify-center items-center">CharchaCafe</span>
+                <MessageSquare className="size-6" />
+            </div>
+                <Menu className="hidden md:flex" />
+            <div className="hidden lg:flex w-60 justify-around">
+                <div className="flex items-center justify-center">
+                    <Settings size={18}/> <span className="text-xs ml-1">Settings</span>
+                </div>
+
+                <div className="flex items-center justify-center">
+                    <User size={18}/> <span className="text-xs ml-1">Profile</span>
+                </div>
+
+                <div className="flex items-center justify-center">
+                    <LogOut size={18}/> <span className="text-xs ml-1">Logout</span>
+                </div>
+            </div>
         </div>
     )
 }
