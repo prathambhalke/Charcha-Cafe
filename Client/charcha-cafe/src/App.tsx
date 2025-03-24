c8a9ffc7326a0b65cda33167fc5fd421b20081d0
Prathamesh Bhalke <98319826+prathambhalke@users.noreply.github.com>
2025-03-24 14:18:07 +0000

    worked on chatcontainer

--- Client/charcha-cafe/src/App.tsx
+++ Client/charcha-cafe/src/App.tsx
@@ -18,7 +18,6 @@ function App() {
     checkAuth();
   }, [checkAuth])
 
-  console.log({ authUser })
   if (isCheckingAuth && !authUser) return (
     <div className="flex items-center justify-center h-screen">
       <Loader className="size-10 animate-spin" />
--- Client/charcha-cafe/src/pages/Home.tsx
+++ Client/charcha-cafe/src/pages/Home.tsx
@@ -1,9 +1,22 @@
-const HomePage = () => {
-  return (
-    <div className="container h-screen w-full flex justify-center items-center">
-      HomePage
-    </div>
-  )
-}
+import ChatContainer from "../components/ChatContainer";
+import NoChatSelected from "../components/NoChatSelected";
+import Sidebar from "../components/Sidebar";
+import { useChatStore } from "../store/useChatStore";
 
-export default HomePage
+const HomePage = () => {
+  const { selectedUser } = useChatStore();
+  return (
+    <div className="h-screen bg-base-200">
+      <div className="flex justify-center items-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8em)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
